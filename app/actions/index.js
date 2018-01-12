b6caf94c2be1a1d54837ec9ad4e30e284cc91133
Willi Krappen <stackola@arch.lan>
2018-01-12 22:17:43 +0100

    remove unneeded code

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,6 +1,3 @@
-//import * as accountActions from './account';
-//import * as offersActions from './offers';
 import * as userActions from './user';
-import * as socketActions from './socket';
 
-export const ActionCreators = Object.assign({}, userActions, socketActions);
+export const ActionCreators = Object.assign({}, userActions);
--- app/containers/AppContainer.js
+++ app/containers/AppContainer.js
@@ -15,44 +15,18 @@ import { bindActionCreators } from 'redux';
 import { navigation,withNavigation, NavigationActions } from 'react-navigation';
 
 
-window.navigator.userAgent = 'ReactNative';
-var socketIOClient = require('socket.io-client');
-var sailsIOClient = require('sails.io.js');
-var io = sailsIOClient(socketIOClient, {
-  transports: ['websocket'] // you need to explicitly tell it to use websockets
-});
-io.sails.useCORSRouteToGetCookie = false
-io.sails.url = 'http://192.168.1.138:1337';
-
-
 
 class AppContainer extends Component<{}> {
 	constructor(props){
 		super(props)
-		this.state={factories:[]};
+		this.state={};
 	}	
 	componentDidMount(){
-		this.props.setSocket(io);
-		this.props.getUser(14);
-		io.socket.get('/user/14', function serverResponded (body, JWR) {
-		  // body === JWR.body
-		  console.log('Sails responded with: ', body);
-		  console.log('with headers: ', JWR.headers);
-		  console.log('and with status code: ', JWR.statusCode);
-		});
-		io.socket.get('/items/getItems', (body, JWR)=>{
-			this.setState({...this.state, factories:body});
-			console.log(body);
-		})
-		io.socket.on('user', (a,b,c)=>{this.props.setUserObject(a.data)});
+
 	}
 	render(){
 		return (<View>
-				<Text>{this.props.user.username}: {this.props.user.points}</Text>
-				<TouchableOpacity style={{height:100}} onPress={()=>this.props.click(14)}><Text style={{textAlign:'center'}}>Click me!</Text></TouchableOpacity>
-				<View>
-					{this.state.factories.map((f)=>{return <Text>{f.production}</Text>})}
-				</View>
+				<Text>It's working. Check your index.js to get started.</Text>
 				</View>);
 	}
 }
